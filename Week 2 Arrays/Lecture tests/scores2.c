42d323a570c2bf853397aca91c462be87924a0dc
Tyke Computing <tykecomputing@gmail.com>
2020-05-05 10:44:24 +0100

    added pset2 problems caesar, readability and substitution git status

--- Week 2 Arrays/Lecture tests/scores2.c
+++ Week 2 Arrays/Lecture tests/scores2.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    /**
+     * The below code is inefficient, as it hard codes the amount of values in the array scores.
     int scores[3];
     //Note the below array, as with most things in the programmming world, are zero-indexed - the counting begins at 0.
     scores[0] = 72;
@@ -10,4 +12,15 @@ int main(void)
     scores[3] = 33;
 
     printf("Average: %i\n", (scores[0] + scores[1] + scores[3]) / 3);
+    **/
+
+   int n =3;
+   int scores[n];
+   scores[0] = 72;
+   scores[1] = 73;
+   scores[2] = 33;
+
+   printf("Average: %ii\n", (scores[0] + scores[1] + scores[2]) / n);
+
+
 }
